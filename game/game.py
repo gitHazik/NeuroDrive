9ad0917fc29a52916e7d19e506713f732ab4601a
gitHazik <hazikkhalid700@gmail.com>
2026-06-02 15:04:59 +0530

    basic car

--- game/game.py
+++ game/game.py
@@ -1,28 +1,32 @@
 import pygame
+from .car import Car
+
 
 class Game:
-
     def __init__(self):
         pygame.init()
 
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.screen = pygame.display.set_mode((800, 600))
+        pygame.display.set_caption("NeuroDrive")
+
         self.clock = pygame.time.Clock()
+        self.car = Car()
 
         self.running = True
 
     def run(self):
-
         while self.running:
 
             for event in pygame.event.get():
-
                 if event.type == pygame.QUIT:
                     self.running = False
 
-            self.screen.fill((20,20,20))
+            self.car.update()
+
+            self.screen.fill((0, 0, 0))
+            self.car.draw(self.screen)
 
             pygame.display.flip()
-
             self.clock.tick(60)
 
         pygame.quit()
