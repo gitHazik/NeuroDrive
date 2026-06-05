f75436f61958eee6eaa5a4c473707094373dd70d
gitHazik <hazikkhalid700@gmail.com>
2026-06-05 13:51:56 +0530

    using stable baseline

---
+++ train.py
@@ -0,0 +1,15 @@
+from stable_baseline3 import PPO
+from game import NeuroDriveEnv
+
+env =  NeuroDriveEnv()
+
+model = PPO("MlpPolicy", env, verbose = 1)
+
+
+print("Starting training..")
+
+
+model.learn(total_timesteps=100000)
+
+model.save("neurodrive_brain")
+
